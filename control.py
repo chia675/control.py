b343a3315ee9777fb3773fb70b601777915f3bae
chia675 <119886339+chia675@users.noreply.github.com>
2023-03-28 14:02:17 +0100

    Add files via upload

---
+++ control.py
@@ -0,0 +1,13 @@
+age = int(input("Enter Your age: ")) # input User age
+
+if age>=18:
+
+    print("You are old enough!")
+
+elif age>=16 and age<18 :
+
+    print("You are almost there!")
+
+else:
+
+    print("You are just too young!")
